377a505f4f84626cb0bd745852904903b9dcbc6c
dev-hack95 <saiprasadtoshatwad@gmail.com>
2023-02-20 08:48:39 +0530

    push

--- prac/embedding_1.go
+++ prac/embedding_1.go
@@ -1,5 +1,21 @@
 package main
 
+import "fmt"
+
+type base struct {
+	num int64
+}
+
+func (b base) describe() string {
+	return fmt.Sprintf("base with num=%v", b.num)
+}
+
+type container struct {
+	// Inserting One struct to another is called embedding of structs
+	base
+	str string
+}
+
 func main() {
 
 }
