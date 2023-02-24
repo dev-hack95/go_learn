9abe37f0ce2c2f5ea9993067d7dbeff63b9dc31c
dev-hack95 <saiprasadtoshatwad@gmail.com>
2023-02-24 19:57:08 +0530

    push

---
+++ prac/channels_1.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+
+// Channels are the pipes that connect concurrent goroutines.
+// You can send values into channels from one goroutine and receive those values into another goroutine.
+func main() {
+	// Create a new channel with make(chan val-type). Channels are typed by the values they convey.
+	messages := make(chan string)
+
+	go func() {
+		messages <- "Hello World"
+	}()
+
+	msg := <-messages
+	fmt.Println(msg)
+}
